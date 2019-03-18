122f9f63689c786533c0d91eb669208ab7377046
remeeliz <R3m3g3l!z>
2019-03-17 20:37:43 -0400

    update config

--- server/controllers/portal.server.controller.js
+++ server/controllers/portal.server.controller.js
@@ -1,7 +1,8 @@
-
 /* Dependencies */
-var mongoose = require('mongoose'), 
-    Listing = require('../models/portal.server.model.js');
+var mongoose = require('mongoose'),
+    Listing = require('../models/portal.server.model.js'),
+    config = require('../config/config');
+
 
 /* Create a listing */
 exports.create = function(req, res) {
@@ -9,14 +10,4 @@ exports.create = function(req, res) {
   /* Instantiate a Listing */
   var portal = new Listing(req.body);
 
-
-  /* Then save the listing */
-  listing.save(function(err) {
-    if(err) {
-      console.log(err);
-      res.status(400).send(err);
-    } else {
-      res.json(listing);
-    }
-  });
 };
