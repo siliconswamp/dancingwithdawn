c59f0759aca5358eb083b0233d79d35fa88718e0
krmont22 <46579960+krmont22@users.noreply.github.com>
2019-03-24 18:26:22 -0500

    Add files via upload
    Add unique Parent Portal dropdown menu

---
+++ content.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("#header").load("header.html");
+});
+
+$(function(){
+    $("#footer").load("footer.html");
+});
+
+$(function(){
+    $("#ppHeader").load("ppHeader.html");
+});
