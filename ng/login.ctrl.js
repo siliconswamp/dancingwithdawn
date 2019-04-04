66785b0f49e2f29647446f9d5633d84f9c11bcf6
Alex Webber <alexrwebber@gmail.com>
2019-04-04 10:24:15 -0400

    FINAL ROUTES, login, reg, admin, and user status functional

--- ng/login.ctrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('app')
-.controller('LoginCtrl', function ($scope, UserSvc) {
-$scope.login = function (username, password) {
-UserSvc.login(username, password)
-    .then(function (user) {
-$scope.$emit('login', user)
-    })
-  }
-})
