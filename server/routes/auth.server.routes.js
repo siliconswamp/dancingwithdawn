f94f7e2d8158a0ee3f5de6a2e3686bc601becf00
Alex Webber <alexrwebber@gmail.com>
2019-04-05 13:10:44 -0400

    Refactored all html views to ejs, updated headers

--- server/routes/auth.server.routes.js
+++ server/routes/auth.server.routes.js
@@ -3,6 +3,101 @@ var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
+// Render home page
+router.get("/", function(req, res){
+    res.render("index", {page: 'index'}); 
+ });
+
+ // Render home page
+router.get("/index", function(req, res){
+    res.render("index", {page: 'index'}); 
+ });
+
+ // Render about me page
+router.get("/aboutme", function(req, res){
+    res.render("aboutme", {page: 'aboutme'}); 
+ });
+
+ //Render admin FAQ editing page
+router.get("/admin_faq", function(req, res){
+    res.render("admin_faq", {page: 'admin_faq'}); 
+});
+
+// Render admin FAQ editing page
+router.get("/admin_text", function(req, res){
+    res.render("admin_text", {page: 'admin_text'}); 
+});
+
+// Render admin portal editing page
+router.get("/adminportal", function(req, res){
+    res.render("adminportal", {page: 'adminportal'}); 
+});
+
+// Render calendar page
+router.get("/calendar", function(req, res){
+    res.render("calendar", {page: 'calendar'}); 
+});
+
+// Render faq page
+router.get("/faq", function(req, res){
+    res.render("faq", {page: 'faq'}); 
+});
+
+// Render forms page
+router.get("/forms", function(req, res){
+    res.render("forms", {page: 'forms'}); 
+});
+
+// Render lessons page
+router.get("/lessons", function(req, res){
+    res.render("lessons", {page: 'lessons'}); 
+});
+
+// Render paypal page
+router.get("/paypal-temp", function(req, res){
+    res.render("paypal-temp", {page: 'paypal-temp'}); 
+});
+
+// Render photo admin page
+router.get("/photos_admin", function(req, res){
+    res.render("photos_admin", {page: 'photos_admin'}); 
+});
+
+// Render photo test page
+router.get("/photos_test", function(req, res){
+    res.render("photos_test", {page: 'photos_test'}); 
+});
+
+// Render photo page
+router.get("/photos", function(req, res){
+    res.render("photos", {page: 'photos'}); 
+});
+
+// Render parent portal forms page
+router.get("/ppForms", function(req, res){
+    res.render("ppForms", {page: 'ppForms'}); 
+});
+
+// Render parent portal history page
+router.get("/ppHistory", function(req, res){
+    res.render("ppHistory", {page: 'ppHistory'}); 
+});
+
+// Render parent portal pictures admins page
+router.get("/ppPictures_admin", function(req, res){
+    res.render("ppPictures_admin", {page: 'ppPictures_admin'}); 
+});
+
+// Render parent portal pictures page
+router.get("/ppPictures", function(req, res){
+    res.render("ppPictures", {page: 'ppPictures'}); 
+});
+
+// Render parent portal forms page
+router.get("/ppSettings", function(req, res){
+    res.render("ppSettings", {page: 'ppSettings'}); 
+});
+
 // Render parent portal
 router.get("/parentportal", function(req, res){
     res.render("parentportal", {page: 'parentportal'}); 
@@ -17,7 +112,7 @@ router.get("/register", function(req, res){
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     if(req.body.adminCode === process.env.ADMIN_CODE) {
-      newUser.isAdmin = true;
+      newUser.isAdmin = false;
     }
     User.register(newUser, req.body.password, function(err, user){
         if(err){
--- server/routes/home.server.routes.js
+++ server/routes/home.server.routes.js
@@ -7,10 +7,6 @@ router.route('/')
     .get(methods.admin_text)
     .post(methods.email);
 
-// Render parent portal
-router.get("/parentportal", function(req, res){
-    res.render("parentportal", {page: 'parentportal'}); 
- });
 router.route('/:_id')
     .post(methods.update_admin_text);
 
