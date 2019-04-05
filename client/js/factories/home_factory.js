923542dfa7ad4ce93a514209fa3c13a03abc6a46
remeeliz <36454978+remeeliz@users.noreply.github.com>
2019-04-05 16:55:55 -0400

    Update home_factory.js
    update post to get

--- client/js/factories/home_factory.js
+++ client/js/factories/home_factory.js
@@ -6,11 +6,11 @@ angular.module('home_collections', []).factory('Home', function($http) {
         },
         contact_form: function(contact_info) {
             //return $http.post('http://localhost:8080/api/contact_form', contact_info);
-            return $http.get('https://dancingwithdawn.herokuapp.com/api/contact_form', contact_info);
+            return $http.post('https://dancingwithdawn.herokuapp.com/api/contact_form', contact_info);
         },
         update_text: function(new_admin_text){
             //return $http.post('http://localhost:8080/api/admin_features/'+new_admin_text._id, new_admin_text);
-            return $http.get('https://dancingwithdawn.herokuapp.com/api/admin_features/'+new_admin_text._id, new_admin_text);
+            return $http.post('https://dancingwithdawn.herokuapp.com/api/admin_features/'+new_admin_text._id, new_admin_text);
         }
     };
     return collections;
