70d8606fbd31119f7bb848e693c2a949ade6c602
remeeliz <R3m3g3l!z>
2019-04-21 03:33:26 -0400

    update css

--- client/js/controllers/home_controller.js
+++ client/js/controllers/home_controller.js
@@ -2,11 +2,29 @@ angular.module('home_collections').controller('HomeController', ['$scope', 'Home
     function ($scope, Home) {
 
         $scope.admin_features = {};
+        $scope.calendar_events = {};
+        $scope.filtered_events = [];
 
         Home.web_text().then(function (response) {
             $scope.admin_features = response.data;
         }, function (error) {
             console.log('Unable to retrieve portal details:', error);
+        });
+
+        Home.get_events().then(function (response){
+            $scope.calendar_events = response.data.items;
+            var c_events = $scope.calendar_events;
+            for(x in c_events)
+            {
+                var event_date = new Date(c_events[x].start.dateTime);
+                var current_date = new Date();
+
+                if(event_date > current_date) {
+                    $scope.filtered_events.push(c_events[x]);
+                }
+            }
+        }, function (error) {
+            console.log('Unable to retrieve calendar events:', error);
         });
 
         $scope.display = {show:0};
--- client/js/factories/home_factory.js
+++ client/js/factories/home_factory.js
@@ -1,8 +1,8 @@
 angular.module('home_collections', []).factory('Home', function($http) {
     var collections = {
         web_text: function(){
-            //return $http.get('http://localhost:8080/api/admin_features');
-            return $http.get('https://dancingwithdawn.herokuapp.com/api/admin_features');
+            return $http.get('http://localhost:8080/api/admin_features');
+            //return $http.get('https://dancingwithdawn.herokuapp.com/api/admin_features');
         },
         contact_form: function(contact_info) {
             //return $http.post('http://localhost:8080/api/contact_form', contact_info);
@@ -11,6 +11,9 @@ angular.module('home_collections', []).factory('Home', function($http) {
         update_text: function(new_admin_text){
             //return $http.post('http://localhost:8080/api/admin_features/'+new_admin_text._id, new_admin_text);
             return $http.post('https://dancingwithdawn.herokuapp.com/api/admin_features/'+new_admin_text._id, new_admin_text);
+        },
+        get_events: function(){
+            return $http.get('https://www.googleapis.com/calendar/v3/calendars/dancingwithdawn352%40gmail.com/events?key=AIzaSyADJbOawrtoPnztOzMta2bDd0Bt2lKtyqs');
         }
     };
     return collections;
