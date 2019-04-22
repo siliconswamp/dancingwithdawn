2b1016e8312208c763f50a363407248c56596398
remeeliz <R3m3g3l!z>
2019-04-22 12:27:37 -0400

    update schema

--- server/models/portal.server.model.js
+++ server/models/portal.server.model.js
@@ -70,61 +70,16 @@ var websiteTextSchema = new Schema({
   }
 },{collection:'admin_features'});
 
-var userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  first_name: {
-    type: String,
-    required: false
-  },
-  last_name: {
-    type: String,
-    required: false
-  },
-  street: {
-    type: String,
-    required: false
-  },
-  city: {
-    type: String,
-    required: false
-  },
-  state: {
-    type: String,
-    required: false
-  },
-  zip: {
-    type: String,
-    required: false
-  },
-  cell_number: {
-    type: String,
-    required: false
-  },
-  alt_number: {
-    type: String,
-    required: false
-  },
-  email: {
-    type: String,
-    required: false
-  }
-},{collection:'users'});
-
 /* Use your schema to instantiate a Mongoose model */
 var Logins = mongoose.model('logins', loginSchema);
 var WebText = mongoose.model('admin_features', websiteTextSchema);
 var FAQSchema = mongoose.model('faq', faqSchema);
 var ClassesSchema = mongoose.model('classes', classesSchema);
-var ParentSchema = mongoose.model('users', ParentSchema);
 
 /* Export the model to make it available to other parts of your Node application */
 module.exports={
   Logins:Logins,
   WebText:WebText,
   FAQSchema:FAQSchema,
-  ClassesSchema:ClassesSchema,
-  ParentSchema:ParentSchema
+  ClassesSchema:ClassesSchema
 };
